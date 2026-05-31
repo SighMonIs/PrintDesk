c48e95f7d0037d5755ce40ba02ab4021f8eed4d0
Simon Reid <simonreidis@gmail.com>
2026-05-31 14:32:35 +1000

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  GAS_URL:   'https://script.google.com/macros/s/AKfycbwX8JuM3jg1zo11mIADvzSv4DTvLGnw6WvdjVvxJ5rcI97fhgJo9GFbmFlJRHD5rfDg/exec',
+  SHEET_URL: 'https://docs.google.com/spreadsheets/d/1MqYD3vQxsptOXetUdOyW3uAH2aleEZP7PgLg88IOTrI/edit?gid=1046151655#gid=1046151655'
+};
