9980625ddc311454384b2ebc710afb5c21efb06e
Simon Reid <simonreidis@gmail.com>
2026-05-31 22:27:47 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
-const CONFIG = {
+// PrintDesk configuration
+// Edit GAS_URL and SHEET_URL below, then upload this file to GitHub.
+// This file overrides any defaults in index.html.
+
+window.CONFIG = {
   GAS_URL:   'https://script.google.com/macros/s/AKfycbwX8JuM3jg1zo11mIADvzSv4DTvLGnw6WvdjVvxJ5rcI97fhgJo9GFbmFlJRHD5rfDg/exec',
   SHEET_URL: 'https://docs.google.com/spreadsheets/d/1MqYD3vQxsptOXetUdOyW3uAH2aleEZP7PgLg88IOTrI/edit?gid=1046151655#gid=1046151655'
 };
