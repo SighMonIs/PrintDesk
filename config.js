23ab91ac51854a1acf5c7d5bc2d1cb600831978b
Simon Reid <simonreidis@gmail.com>
2026-06-02 06:15:45 +1000

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // This file overrides any defaults in index.html.
 
 window.CONFIG = {
-  GAS_URL:   'https://script.google.com/macros/s/AKfycbyabzMgqGUIFlMbXJJvvDHdzYMOGXVfizNpkVPnKgtTy54uduju4QTAC9yDeDrEy6bw/exec',
+  GAS_URL:   'https://script.google.com/macros/s/AKfycbyGgfG2MJg7_WzXZLcyKnnwalLdNkGP1Fr8fO396fuTe6sRwOfEufTYRIYR0Bd3wGPG/exec',
   SHEET_URL: 'https://docs.google.com/spreadsheets/d/1MqYD3vQxsptOXetUdOyW3uAH2aleEZP7PgLg88IOTrI/edit?gid=1046151655#gid=1046151655'
 };
