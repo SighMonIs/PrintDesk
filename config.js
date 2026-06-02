3a2374fed4f7fd00710e53b13c729cc8a50bf813
Simon Reid <simonreidis@gmail.com>
2026-06-02 15:53:10 +1000

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Edit these values then upload to GitHub alongside index.html
 
 window.CONFIG = {
-  SUPABASE_URL: 'https://brcwodrprpkxerxlpfgkd.supabase.co',
+  SUPABASE_URL: 'https://brcwodrrpkxerxlpfgkd.supabase.co',
   SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJyY3dvZHJycGt4ZXJ4bHBmZ2tkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzNDA3MzgsImV4cCI6MjA5NTkxNjczOH0.T9m0gtP1Xp9PB8u8FnS0-9_a2Z2EHYrW4-NQbU6Z9tI'
 };
